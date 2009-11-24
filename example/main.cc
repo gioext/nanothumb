c6fd3d31e75a6897b8be4a1afee557340e34627c
Kazuki UCHIDA <gioext@gmail.com>
2009-11-25 02:22:07 +0900

    todo

--- example/main.cc
+++ example/main.cc
@@ -5,5 +5,12 @@ int main() {
     nanothumb::Gic *b = nanothumb::convert(a, 0.5);
     nanothumb::gic_write_image(b, "b.jpg", 90);
 
+    // TODO
+    // nanothumb::create("a.jpg", "b.jpg", 0.5, 90);
+    //
+    // nanothumb::Image img = nanothumb::Image("a.jpg");
+    // img.resize(0.5);
+    // img.write("b.jpg", 90);
+    //
     return 0;
 }
